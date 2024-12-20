5c4bfb58a44475fdbce4aae475bceb35043f68ec
sigmachebypel228 <timuryablokov2012@gmail.com>
2024-12-20 21:39:22 +0300

    вав

--- main.py
+++ main.py
@@ -8,25 +8,11 @@ import time
 def set_status(text):
     global text_id
     canvas.itemconfig(text_id, text=text)
-def pause_toggle():
-    global pause
-    if not game_over:
-        pause = not pause
-        if pause:
-            set_status("Пауза")
-        else:
-            set_status("Вперед!")
-def menu_toggle():
-    global menu_mode
-    menu_mode = not menu_mode
-    if menu_mode:
-        menu_show()
-    else:
-        menu_hide()
+
 
 
 def key_handler(event):
-    global KEY_PLAYER1, KEY_PLAYER2, KEY_PAUSE, KEY_ESC, KEY_ENTER, SPEED, x1, x2, game_over, menu_mode, menu_current_index
+    global KEY_PLAYER1, KEY_PLAYER2,  SPEED, x1, x2, game_over
 
     # Управление игроком 1
     if event.keycode == KEY_PLAYER1:
@@ -39,25 +25,6 @@ def key_handler(event):
         canvas.coords(player2, x2, y2, x2 + player_size, y2 + player_size)
 
     # Пауза
-    elif event.keycode == KEY_PAUSE:
-        pause_toggle()
-
-    # Выход в меню
-    elif event.keycode == KEY_ESC:
-        if not menu_mode:
-            menu_toggle()
-
-    # Выбор опции в меню
-    elif event.keycode == KEY_ENTER and menu_mode:
-        menu_enter()
-
-    # Перемещение вверх в меню
-    elif event.keycode == KEY_UP and menu_mode:
-        menu_up()
-
-    # Перемещение вниз в меню
-    elif event.keycode == KEY_DOWN and menu_mode:
-        menu_down()
 
     # Проверка завершения гонки
     check_finish()
@@ -68,98 +35,9 @@ def check_finish():
         set_status(f'Победил игрок {"первый" if x1 > x2 else "второй"}!')
 
 
-def menu_enter():
-    global menu_current_index, menu_options
-    option = menu_options[menu_current_index]
 
-    if option == 'Возврат в игру':
-        menu_toggle()
-    elif option == 'Новая игра':
-        game_new()
-        menu_hide()
-    elif option == 'Сохранить':
-        game_save()
-    elif option == 'Загрузить':
-        game_load()
-    elif option == 'Выход':
-        game_exit()
-def game_new():
-    global x1, x2, game_over, pause, menu_mode
-    x1, y1 = 50, 50
-    x2, y2 = x1, y1 + player_size + 100
-    canvas.coords(player1, x1, y1, x1 + player_size, y1 + player_size)
-    canvas.coords(player2, x2, y2, x2 + player_size, y2 + player_size)
-    game_over = False
-    pause = False
-    menu_mode = False
-    set_status("Вперед!")
-def game_resume():
-    global menu_mode
-    menu_mode = False
-    menu_hide()
-def game_save():
-    data = {
-        'x1': x1,
-        'y1': y1,
-        'x2': x2,
-        'y2': y2,
-        'game_over': game_over,
-        'pause': pause,
-        'menu_mode': menu_mode,
-        'menu_current_index': menu_current_index
-    }
-    with open('save.pkl', 'wb') as f:
-        dump(data, f)
-    set_status("Игра сохранена!")
-def game_load():
-    global x1, y1, x2, y2, game_over, pause, menu_mode, menu_current_index
-    try:
-        with open('save.pkl', 'rb') as f:
-            data = load(f)
-        x1 = data['x1']
-        y1 = data['y1']
-        x2 = data['x2']
-        y2 = data['y2']
-        game_over = data['game_over']
-        pause = data['pause']
-        menu_mode = data['menu_mode']
-        menu_current_index = data['menu_current_index']
-        canvas.coords(player1, x1, y1, x1 + player_size, y1 + player_size)
-        canvas.coords(player2, x2, y2, x2 + player_size, y2 + player_size)
-        if menu_mode:
-            menu_show()
-        else:
-            menu_hide()
-        set_status("Игра загружена!")
-    except FileNotFoundError:
-        set_status("Файл сохранения не найден.")
-def game_exit():
-    window.destroy()
-def menu_show():
-    for i in range(len(menu_options)):
-        canvas.itemconfig(menu_options_id[i], state=NORMAL)
-def menu_hide():
-    for id in menu_options_id:
-        canvas.itemconfig(id, state=HIDDEN)
-def menu_up():
-    global menu_current_index
-    menu_current_index = (menu_current_index - 1) % len(menu_options)
-    menu_update()
-def menu_down():
-    global menu_current_index
-    menu_current_index = (menu_current_index + 1) % len(menu_options)
-    menu_update()
-def menu_update():
-    for i in range(len(menu_options)):
-        color = 'green' if i == menu_current_index else 'black'
-        canvas.itemconfig(menu_options_id[i], fill=color)
-def menu_create(canvas):
-    global menu_options_id
-    y = 200
-    for option in menu_options:
-        id = canvas.create_text(game_width // 2, y, text=option, font=('Arial', '20'), fill='black')
-        menu_options_id.append(id)
-        y += 40
+
+
 game_width = 800
 game_height = 800
 menu_mode = True
@@ -196,7 +74,7 @@ window.title('DMEC')
 
 canvas = Canvas(window, width=game_width, height=game_height, bg='white')
 canvas.pack()
-menu_create(canvas)
+
 player1 = canvas.create_rectangle(x1,
                                   y1,
                                   x1 + player_size,
